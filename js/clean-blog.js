5e62d92a5ec508395beb9ef30c85d90b56e2b66c
JDStraughan <jdstraughan@gmail.com>
2015-06-11 16:58:06 -0500

    Updates per Google

--- js/clean-blog.js
+++ js/clean-blog.js
@@ -1,11 +1,1 @@
-/*!
- * Clean Blog v1.0.0 (http://startbootstrap.com)
- * Copyright 2014 Start Bootstrap
- * Licensed under Apache 2.0 (https://github.com/IronSummitMedia/startbootstrap/blob/gh-pages/LICENSE)
- */
-
-// Tooltip Init
-$(function() {
-    $("[data-toggle='tooltip']").tooltip();
-});
-
+$(function(){$("[data-toggle='tooltip']").tooltip();});
